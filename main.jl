88d2df95c95854557244fb6efbc5ecddd9804147
Herbin Clément <leever@hotmail.fr>
2022-02-12 19:45:08 +0100

    add multi-threading static when compute color

--- main.jl
+++ main.jl
@@ -1,8 +1,9 @@
-# @time using Images
+@time using Images
 # @time using VideoIO
 
-function color(z::ComplexF64, c::ComplexF64, limit::Float64, depth::Int)::Array{UInt8, 1}
+function color(c::ComplexF64, limit::Float64, depth::Int)::UInt8
     count = 0
+    z = complex(0., 0.)
     for _ in 1:depth
         z = z * z + c
         if norm(z) > limit
@@ -10,26 +11,26 @@ function color(z::ComplexF64, c::ComplexF64, limit::Float64, depth::Int)::Array{
         end
         count += 1
     end
-    return [trunc(UInt8, 255 * count / depth), trunc(UInt8, 255 * count / depth), trunc(UInt8, 255 * count / depth)]
+    return trunc(UInt8, 255 * count / depth)
 end
 
 function norm(c::ComplexF64)::Float64
     return sqrt(real(c) * real(c) + imag(c) * imag(c))
 end
 
-function mandelbrot(c::ComplexF64, xmin::Float64, xmax::Float64, ymin::Float64, ymax::Float64, w::Int64, h::Int64, depth::Int64, limit::Float64)::Array{UInt8, 3}
+function mandelbrot(xmin::Float64, xmax::Float64, ymin::Float64, ymax::Float64, w::Int64, h::Int64, depth::Int64, limit::Float64)::Array{UInt8, 2}
     xshift = (xmax - xmin) / (w - 1)
     yshift = (ymax - ymin) / (h - 1)
     img = Array{UInt8,2}(undef, h, w)
-    y = ymin
-    for i in 1:h
+    ylength = ymax - ymin
+    Threads.@threads for i in 1:h
+        y = ymin + i * yshift
         x = xmin
         for j in 1:w
-            z = complex(x, y)
-            img[j, i] = color(z, c, limit, depth)
+            c = complex(x, y)
+            img[i, j] = color(c, limit, depth)
             x += xshift
         end
-        y += yshift
     end
     return img
 end
@@ -41,18 +42,18 @@ function printComplexArray(z_n::Array{ComplexF64})
 end
 
 function main()
+    println("Number of thread: $(Threads.nthreads())")
     nb_img = 1
     xmin = -2.0
     xmax = 2.0
     ymin = -2.0
     ymax = 2.0
-    w = 2500
-    h = 2500
+    w = 8000
+    h = 8000
     depth = 100
-    limit = 5000.0
-    i = 0.75
-    j = 0.25
-    @time img = mandelbrot(complex(i, j), xmin, xmax, ymin, ymax, w, h, depth, limit)
+    limit = 5.0
+    @time img = mandelbrot(xmin, xmax, ymin, ymax, w, h, depth, limit)
+    save("c.png", img)
     # imgs = Array{Array{UInt8, 2}, 1}(undef, nb_img)
     # for i in -1:0.25:1
     #     for j in -1:0.25:1
@@ -62,10 +63,9 @@ function main()
     #         end
     #     end
     # end
-    # save("img/c.png", img)
     # save("rgb.png", colorview(RGB, rand(3,256,256)))
-    println(typeof(rand(3, 256, 256)))
-    println(typeof(img))
+    # println(typeof(rand(3, 256, 256)))
+    # println(typeof(img))
 end
 
 main()
