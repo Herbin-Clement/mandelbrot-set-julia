607eb373ed1f0f91162d0a517eb6d6220ead540a
Herbin Clément <leever@hotmail.fr>
2022-02-13 22:18:16 +0100

    can have rgb color

--- main.jl
+++ main.jl
@@ -3,7 +3,7 @@
 @time using ProgressMeter
 @time using FileIO
 
-function color(c::ComplexF64, limit::Float64, depth::Int)::UInt8
+function color(c::ComplexF64, limit::Float64, depth::Int)::Tuple{Float64, Float64, Float64}
     count = 0
     z = complex(0., 0.)
     for _ in 1:depth
@@ -13,24 +13,24 @@ function color(c::ComplexF64, limit::Float64, depth::Int)::UInt8
         end
         count += 1
     end
-    return trunc(UInt8, 255 * count / depth)
+    return 255 * count / (depth * 255), 255 * count / (depth * 255), 0.
 end
 
 function norm(c::ComplexF64)::Float64
     return sqrt(real(c) * real(c) + imag(c) * imag(c))
 end
 
-function mandelbrot(xmin::Float64, xmax::Float64, ymin::Float64, ymax::Float64, w::Int64, h::Int64, depth::Int64, limit::Float64)::Array{UInt8, 2}
+function mandelbrot(xmin::Float64, xmax::Float64, ymin::Float64, ymax::Float64, w::Int64, h::Int64, depth::Int64, limit::Float64)::Array{Float64, 3}
     xshift = (xmax - xmin) / (w - 1)
     yshift = (ymax - ymin) / (h - 1)
-    img = Array{UInt8,2}(undef, h, w)
+    img = Array{Float64, 3}(undef, h, w, 3)
     ylength = ymax - ymin
     Threads.@threads for i in 1:h
         y = ymin + i * yshift
         x = xmin
         for j in 1:w
             c = complex(x, y)
-            img[i, j] = color(c, limit, depth)
+            img[i, j, 1], img[i, j, 2], img[i, j, 3] = color(c, limit, depth)
             x += xshift
         end
     end
@@ -83,7 +83,6 @@ end
 function processImage(dir::String, w::Int, h::Int, zlimit::Float64, x_center::Float64, y_center::Float64, limit::Float64, scale::Float64, space::Float64)
     num_img = 1
     nb_img = getNumberOfImage(space, scale, limit)
-    imgs = Array{Array{UInt8, 2}, 1}(undef, nb_img)
     while space > limit
         space = scale * space
         xmin = - space + x_center
@@ -93,18 +92,18 @@ function processImage(dir::String, w::Int, h::Int, zlimit::Float64, x_center::Fl
         xlims = xmax - xmin
         depth = trunc(Int, 50 + log10(((4 / abs(xlims)))) ^ 5)
         print("iter: $depth ")
-        @time imgs[num_img] = mandelbrot(xmin, xmax, ymin, ymax, w, h, depth, zlimit)
-        save("$dir/$num_img.png", imgs[num_img])
+        @time img = mandelbrot(xmin, xmax, ymin, ymax, w, h, depth, zlimit)
+        save("$dir/$num_img.png", img)
         num_img += 1
     end
 end
 
 function main()
     println("Number of thread: $(Threads.nthreads())")
-    dir = "img2"
-    if Base.Filesystem.ispath("img2")
-        Base.Filesystem.rm("img2", recursive=true)
-    end
+    dir = "img"
+    # if Base.Filesystem.ispath(dir)
+    #     Base.Filesystem.rm(dir, recursive=true)
+    # end
     w = 800
     h = 800
     zlimit = 5.0
@@ -112,9 +111,9 @@ function main()
     y_center = 0.1
     space = 2.0
     limit = 0.0001
-    scale = 0.95
-    processImage(dir, w, h, zlimit, x_center, y_center, limit, scale, space)
-    # processVideo(string(pwd(), "/", dir))
+    scale = 0.99
+    # processImage(dir, w, h, zlimit, x_center, y_center, limit, scale, space)
+    processVideo(string(pwd(), "/", dir))
 end
 
 main()
