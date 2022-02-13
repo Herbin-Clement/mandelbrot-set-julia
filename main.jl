35213888ee0e58461af3dc641ca964f171c6c360
Herbin Clément <leever@hotmail.fr>
2022-02-13 16:34:17 +0100

    can get mp4 video

--- main.jl
+++ main.jl
@@ -1,5 +1,7 @@
 @time using Images
-# @time using VideoIO
+@time using VideoIO
+@time using ProgressMeter
+@time using FileIO
 
 function color(c::ComplexF64, limit::Float64, depth::Int)::UInt8
     count = 0
@@ -41,31 +43,75 @@ function printComplexArray(z_n::Array{ComplexF64})
     end
 end
 
+function getNumberOfImage(space::Float64, scale::Float64, limit::Float64)::Int
+    count = 0
+    while space > limit
+        space = scale * space
+        count += 1
+    end
+    return count
+end
+
 function main()
     println("Number of thread: $(Threads.nthreads())")
-    nb_img = 1
-    xmin = -2.0
-    xmax = 2.0
-    ymin = -2.0
-    ymax = 2.0
-    w = 8000
-    h = 8000
-    depth = 100
-    limit = 5.0
-    @time img = mandelbrot(xmin, xmax, ymin, ymax, w, h, depth, limit)
-    save("c.png", img)
-    # imgs = Array{Array{UInt8, 2}, 1}(undef, nb_img)
-    # for i in -1:0.25:1
-    #     for j in -1:0.25:1
-    #         if i != 0 && j != 0 
-    #             @time img = mandelbrot(complex(i, j), xmin, xmax, ymin, ymax, w, h, depth, limit)
-    #             save("img/c_$i-$j.png", img)
-    #         end
-    #     end
+    dir = "/img"
+    # if Base.Filesystem.ispath("img")
+    #     Base.Filesystem.rm("img", recursive=true)
     # end
-    # save("rgb.png", colorview(RGB, rand(3,256,256)))
-    # println(typeof(rand(3, 256, 256)))
-    # println(typeof(img))
+    # w = 2500
+    # h = 2500
+    # depth = 1000
+    # limit = 5.0
+    # num_img = 1
+    # x_center = - 0.75
+    # y_center = 0.1
+    # space = 2.0
+    # lims = 0.0001
+    # scale = 0.95
+    # nb_img = getNumberOfImage(space, scale, lims)
+    # imgs = Array{Array{UInt8, 2}, 1}(undef, nb_img)
+    # while space > lims
+    #     space = scale * space
+    #     xmin = - space + x_center
+    #     xmax = space + x_center
+    #     ymin = - space + y_center
+    #     ymax = space + y_center
+    #     xlims = xmax - xmin
+    #     depth = trunc(Int, 50 + log10(((4 / abs(xlims)))) ^ 5)
+    #     print("iter: $depth ")
+    #     @time imgs[num_img] = mandelbrot(xmin, xmax, ymin, ymax, w, h, depth, limit)
+    #     save("img/$num_img.png", imgs[num_img])
+    #     num_img += 1
+    # end
+    processVideo(string(pwd(), dir))
+end
+
+function processVideo(dir::String)
+    # imgnames = filter(x->occursin(".png",x), readdir(dir)) # Populate list of all .pngs
+    # intstrings =  map(x->split(x,".")[1],imgnames) # Extract index from filenames
+    # p = sortperm(parse.(Int,intstrings)) #sort files numerically
+    # imgstack = []
+    # for imgname in imgnames[p]
+    #     push!(imgstack, load(string(pwd(), "/img/", imgname)))
+    # end
+    # encoder_options = (crf=23, preset="medium")
+    # props = [:priv_data => ("crf"=>"22","preset"=>"medium")]
+    # save("video.mp4", imgstack, framerate=30, encoder_options=encoder_options)
+
+    imgnames = filter(x->occursin(".png",x), readdir(dir)) # Populate list of all .pngs
+    intstrings =  map(x->split(x,".")[1], imgnames) # Extract index from filenames
+    p = sortperm(parse.(Int, intstrings)) #sort files numerically
+    imgnames = imgnames[p]
+
+    encoder_options = (crf=23, preset="medium")
+
+    firstimg = load(joinpath(dir, imgnames[1]))
+    open_video_out("video.mp4", firstimg, framerate=24, encoder_options=encoder_options) do writer
+        @showprogress "Encoding video frames.." for i in eachindex(imgnames)
+            img = load(joinpath(dir, imgnames[i]))
+            write(writer, img)
+        end
+    end
 end
 
 main()
